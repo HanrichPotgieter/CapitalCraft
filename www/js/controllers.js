30eb50351dfee657c27533981e2df30aa7d19c25
FrikkieSnyman <frikkie.snyman@live.co.za>
2015-08-22 21:16:37 +0200

    Updated UX, ratings

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AccountCtrl', function($scope, $ionicPopup, $timeout, $state, User) {
+.controller('AccountCtrl', function($scope, $ionicPopup, $timeout, $state, $ionicLoading, User) {
 	$scope.settings = {
 		enableFriends: true
 	};
@@ -31,6 +31,13 @@ $scope.showPopup = function(url,t) {
 		]
 	}).then(function(res) {
 		if(!res.first){
+      $ionicLoading.show({
+        content: 'Loading',
+        animation: 'fade-in',
+        showBackdrop: true,
+        maxWidth: 200,
+        showDelay: 0
+      });
 			var ref = new Firebase("https://capitalcraft.firebaseio.com");
 			ref.authWithPassword({
 			  email    : res.email,
@@ -41,6 +48,7 @@ $scope.showPopup = function(url,t) {
 			  } else {
 			    console.log("Authenticated successfully with payload:", authData);
 			    User.set(authData);
+          $ionicLoading.hide();
 			    $state.go("tab.beers");
 			  }
 			});
@@ -94,8 +102,8 @@ $scope.showPopup = function(url,t) {
 
 .controller('BeerDetailCtrl', function($scope, $ionicPopup, $stateParams, $state, Beers, User){
   $scope.beer = JSON.parse($stateParams.beer);
-  $scope.rate = 3;
-  $scope.max = 5;
+  $scope.bate = $scope.beer.avgRating;
+  $scope.max = 10;
   $scope.isLoggedIn = function() {
     return User.isLoggedIn();
   };
@@ -103,46 +111,54 @@ $scope.showPopup = function(url,t) {
   $scope.rate = function() {
     $scope.data = {};
     canceled = false;
-    var myPopup = $ionicPopup.show({
-      template : '<input type="text" ng-model="data.rating">',
-      title : 'Rate ' + $scope.beer.title,
-      subtitle : 'What do you think of ' + $scope.title.beer + '?',
-      scope : $scope,
-      buttons : [
-        {
-          text : 'Cancel',
-          onTap : function(e) {
-            canceled = true;
-          }
-        },
-        {
-          text : '<b>Save</b>',
-          type : 'button-positive',
-          onTap: function(e) {
-            if (!$scope.data.rating) {
-              e.preventDefault();
-            } else {
-              return $scope.data.rating;
+    var ref = new Firebase('https://capitalcraft.firebaseio.com/ratings/'+User.get().uid+'/'+$scope.beer.$id);
+    ref.child('rating');
+    ref.once('value', function(rating){
+      if (rating.val() !== null) {
+        $scope.data.rating = rating.val().rating;
+      }
+      var myPopup = $ionicPopup.show({
+        template : '<rating ng-model="data.rating" max="max" style="font-size:10px;!important"></rating>',
+        title : 'Rate ' + $scope.beer.title,
+        subtitle : 'What do you think of ' + $scope.title.beer + '?',
+        scope : $scope,
+        buttons : [
+          {
+            text : 'Cancel',
+            onTap : function(e) {
+              canceled = true;
+            }
+          },
+          {
+            text : '<b>Save</b>',
+            type : 'button-positive',
+            onTap: function(e) {
+              if (!$scope.data.rating) {
+                e.preventDefault();
+              } else {
+                return $scope.data.rating;
+              }
             }
           }
-        }
-      ]
+        ]
+      });
+      myPopup.then(function(res) {
+        if (canceled !== true) {
+            var user = User.get();
+            var ref = new Firebase('https://capitalcraft.firebaseio.com/ratings/'+user.uid+'/'+$scope.beer.$id);
+            ref.child('rating').set(res);
+    
+            var beer = Beers.get($scope.beer.$id);
+            beer.once('value',function(data) {
+              var curAvg = data.val().avgRating * parseInt(data.val().numRates);
+              var newAvg = (curAvg + parseInt(res)) / (parseInt(data.val().numRates) + 1);
+              beer.child('avgRating').set(newAvg);
+              beer.child('numRates').set(parseInt(data.val().numRates) + 1);
+            });
+          }
+      });  
     });
-    myPopup.then(function(res) {
-      if (canceled !== true) {
-          var user = User.get();
-          var ref = new Firebase('https://capitalcraft.firebaseio.com/ratings/'+user.uid+'/'+$scope.beer.$id);
-          ref.child('rating').set(res);
-  
-          var beer = Beers.get($scope.beer.$id);
-          beer.once('value',function(data) {
-            var curAvg = data.val().avgRating * parseInt(data.val().numRates);
-            var newAvg = (curAvg + parseInt(res)) / (parseInt(data.val().numRates) + 1);
-            beer.child('avgRating').set(newAvg);
-            beer.child('numRates').set(parseInt(data.val().numRates) + 1);
-          });
-        }
-    });
+    
   };
 })
 
